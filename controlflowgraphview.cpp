2f601350fbb005ea1f6df4ff48476d555da6ca26
Csaba Kiraly <csaba.kiraly@gmail.com>
2014-08-24 20:02:36 +0200

    fix kgraphviewerpart creation
    fix from Sandro Andrade <sandroandrade@kde.org>

--- controlflowgraphview.cpp
+++ controlflowgraphview.cpp
@@ -52,7 +52,7 @@ m_graphLocked(false)
     KLibFactory *factory = KLibLoader::self()->factory("kgraphviewerpart");
     if (factory)
     {
-        m_part = factory->create<KParts::ReadOnlyPart>(this);
+        m_part = factory->create<KParts::ReadOnlyPart>("kgraphviewerpart", this);
         if (m_part)
         {
             QMetaObject::invokeMethod(m_part, "setReadWrite");
